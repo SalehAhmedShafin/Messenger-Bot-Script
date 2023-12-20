18268e6b0c2fa386bf67837e81230a74ac3d2db9
Saleh Ahmed Shafin <70691546+SalehAhmedShafin@users.noreply.github.com>
2023-12-20 11:20:47 +0600

    Update messenger_bot.py

--- messenger_bot.py
+++ messenger_bot.py
@@ -1,17 +1,18 @@
 import time
 import pyautogui
 
-def send_message(message, count):
-    print("Switch to Messenger and focus on the chat input field...")
-    time.sleep(5)  # Give you some time to switch to Messenger
+def send_messages(base_message, count):
+    print("Switch to Apps and focus on the chat input field...")
+    time.sleep(5)
 
-    for _ in range(count):
+    for i in range(1, count + 1):
+        message = f"{base_message} {i}"
         pyautogui.typewrite(message)
         pyautogui.press("enter")
-        time.sleep(1)  # Adjust the sleep duration if needed
+        time.sleep(1) 
 
 if __name__ == "__main__":
-    message_to_send = "Hey, me Saleh Ahmed Shafin"
+    base_message_to_send = "I love you"
     messages_count = 100
 
-    send_message(message_to_send, messages_count)
+    send_messages(base_message_to_send, messages_count)
